ec17a59fd33732cf8772dbb1e306a0a6eabdd40a
Zach Frank <mzmfrank@gmail.com>
2022-01-01 20:52:26 -0500

    January 1st 2022

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 # Initialize Libraries Necessary for API and GET pulls
 import requests
-import json
 
-link = ''
+class Convert:
+    def __init__(self, coinQuantity, coin, currency):
+        self.coinQuantity = coinQuantity
+        self.coin = coin
+        self.currency = currency
+    def conversionRate (self):
+        link = 'https://cex.io/api/ticker/' + self.coin + '/' + self.currency
+        r = requests.get(link)
+        END = r.json()['last']
+        return float(END)
+
+    def convertMain (self):
+        convert = self.coinQuantity * self.conversionRate()
+        print('$' + str(round((convert), 2)))
+
+
 
 
 class Bitcoin:
@@ -11,11 +25,18 @@ class Bitcoin:
         self.addr = addr
 
     def Pull(self):
-        link = 'https://blockchain.info/q/addressbalance/' + self.addr
-        r = requests.get(link)
+        linkAddr = 'https://blockchain.info/q/addressbalance/' + self.addr
+        r = requests.get(linkAddr)
         satoshi = r.text
         totalBtc = int(satoshi) / (10**8)
-        print(totalBtc)
+        print(str(totalBtc) + 'BTC')
+        BtcConvert = Convert(totalBtc, 'BTC', 'USD')
+        BtcConvert.convertMain()
+
+        #totalUSD = round(conversionRate('BTC') * totalBtc, 2)
+        #print('$' + str(totalUSD))
+
+
 
 
 class Ethereum:
@@ -24,12 +45,15 @@ class Ethereum:
         self.addr = addr
 
     def Pull(self):
-        link = 'https://api.blockcypher.com/v1/eth/main/addrs/' + self.addr
-        r = requests.get(link)
-        wei = (r.json()['balance'])
+        linkAddr = 'https://api.blockcypher.com/v1/eth/main/addrs/' + self.addr
+        r = requests.get(linkAddr)
+        wei = r.json()['balance']
         print(wei)
         totalEth = wei / (10**18)
-        print(totalEth)
+        print(str(totalEth) + 'ETH')
+        EthConvert = Convert(totalEth, 'ETH', 'USD')
+        EthConvert.convertMain()
+
 
 # lead = input("What coin: ")
 addrB = Bitcoin('bc1qxy2kgdygjrsqtzq2n0yrf2493p83kkfjhx0wlh')
