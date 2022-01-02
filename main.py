39ec8c053716575ec6516a3359b96a45d318d1d0
Zach Frank <mzmfrank@gmail.com>
2022-01-01 22:29:55 -0500

    January 1st 2022

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import datetime
 import requests
 import json
 
-
 BTCWord = ['BTC', 'btc', 'Bitcoin', 'bitcoin']
 ETHWord = ['ETH', 'eth' 'Ethereum', 'ethereum']
 SOLWord = ['SOL', 'sol', 'solana', 'Solana']
+
+
 class Convert:
     def __init__(self, coinQuantity, coin, currency):
         self.coinQuantity = coinQuantity
@@ -84,28 +85,32 @@ class Solana:
 class Setup:
     def __init__(self, blank):
         self.blank = blank
+
     def welcome(self):
         print(datetime.date.today())
         print('Welcome to the Multi-coin Crypto Wallet')
         self.DetCoin()
+
     def DetCoin(self):
         leadInput = input("To continue please enter the coin you would like to search for: ")
         if leadInput in BTCWord:
             self.InputBTC()
+
     def InputBTC(self):
         addrB = Bitcoin(input("What is you Bitcoin Public Key: "))
         addrB.Pull()
+
     def InputETH(self):
         addrE = Ethereum(input("What is you Ethereum Public Key: "))
         addrE.Pull()
+
     def InputSOL(self):
         addrS = Solana(input("What is you Solana Public Key: "))
         addrS.Pull()
 
+
 start = Setup('')
 start.welcome()
-
-
 
 """
 addrB = Bitcoin('bc1qxy2kgdygjrsqtzq2n0yrf2493p83kkfjhx0wlh')
