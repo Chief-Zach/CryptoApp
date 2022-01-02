05a252d266fe210dfdf0c0519570b0dc08cd0ab1
Zach Frank <mzmfrank@gmail.com>
2022-01-01 21:33:20 -0500

    January 1st 2022

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Initialize Libraries Necessary for API and GET pulls
 import requests
-
+import json
 class Convert:
     def __init__(self, coinQuantity, coin, currency):
         self.coinQuantity = coinQuantity
@@ -33,9 +33,6 @@ class Bitcoin:
         BtcConvert = Convert(totalBtc, 'BTC', 'USD')
         BtcConvert.convertMain()
 
-        #totalUSD = round(conversionRate('BTC') * totalBtc, 2)
-        #print('$' + str(totalUSD))
-
 
 
 
@@ -54,9 +51,36 @@ class Ethereum:
         EthConvert = Convert(totalEth, 'ETH', 'USD')
         EthConvert.convertMain()
 
+class Solana:
+    def __init__(self, addr):
+        self.addr = addr
+
+    def setup(self):
+        url = 'https://api.mainnet-beta.solana.com'
+        headers = {'content-type': 'application/json'}
+
+        result = {
+            "jsonrpc": "2.0",
+            "method": "getBalance",
+            "params": [self.addr],
+            "id": 0,
+        }
+        r = requests.post(url, data=json.dumps(result), headers=headers)
+        return r
+
+
+    def Pull(self):
+        r = self.setup()
+        lamports = r.json()['result']['value']
+        totalSol = lamports / (10**9)
+        print(totalSol)
+        SolConvert = Convert(totalSol, 'SOL', 'USD')
+        SolConvert.convertMain()
 
 # lead = input("What coin: ")
 addrB = Bitcoin('bc1qxy2kgdygjrsqtzq2n0yrf2493p83kkfjhx0wlh')
 addrE = Ethereum('738d145faabb1e00cf5a017588a9c0f998318012')
+addrS = Solana('FrnE6mc35yb6mEGFDAfRStrT2WAy46SVxzGjqsbcNcnY')
 
-addrB.Pull()
+
+addrS.Pull()
